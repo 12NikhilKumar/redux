10eb5fad9396e04d5e09ef49ba7b6202db0689c8
Nikhil kumar <egkumarnikhil16@gmail.com>
2021-12-29 23:34:25 +0530

    coding evalution 2

--- src/evalution/navbar.jsx
+++ src/evalution/navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function Navbar(){
     return(
-        <div style={{display: 'flex', padding:'1rem',gap: '5rem',width:'100%',height:'50px',backgroundColor:'aqua',overflow: 'hidden',position:'fixed',top:'0',justifyContent: 'center'}}>
+        <div style={{display: 'flex', padding:'1rem',gap: '5rem',width:'100%',height:'50px',backgroundColor:'black',color:'white',fontSize:'25px',fontWeight:'bold',overflow: 'hidden',position:'fixed',top:'0',justifyContent: 'center'}}>
             <Link to="/">Home</Link>
             <Link to="/add">Cart</Link>
         </div>
